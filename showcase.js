b43323eafed4705f2a77d0511e4c33dc6d31da35
klpod221 <klpod221@gmail.com>
2025-11-25 16:24:37 +0700

    chore: switch KemiUI import from CDN to local source.

--- showcase.js
+++ showcase.js
@@ -1,4 +1,4 @@
-import * as KemiUI from "https://cdn.jsdelivr.net/npm/@klpod221/kemi-ui/dist/kemi-ui.js";
+import * as KemiUI from "./src/index.ts";
 window.KemiUI = KemiUI;
 
 // Global helper functions for showcase interactions
